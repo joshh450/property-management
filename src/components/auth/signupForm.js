9240117abb43a9ea9b0856ad647e809880ee69b0
joshh450 <joshh450@yahoo.com>
2018-10-23 11:17:59 -0600

    built signup form

---
+++ src/components/auth/signupForm.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import  { reduxForm, Field } from 'redux-form';
+
+import FormTitle from '../formTitle';
+import { FormInput, FormButton } from '../formFields';
+import TextLink from '../textLink';
+
+class SignupForm extends Component {
+    render() {
+        return (
+            <form className='sign-up-form'>
+                <FormTitle className='sign-up-form__title' text='New User' />
+                <Field 
+                className='sign-up-form__fullname' 
+                placeholder='Enter Your Full Name' 
+                component={FormInput} 
+                name='fullname' 
+                title="Full Name"
+                type='text' />
+                <Field 
+                className='sign-up-form__unit' 
+                placeholder='Enter Unit #' 
+                component={FormInput} 
+                name='unit' 
+                title="Unit #"
+                type='text' />
+                <Field 
+                className='sign-up-form__email' 
+                placeholder='Enter Email' 
+                component={FormInput} 
+                name='email' 
+                title="Email"
+                type='email' />
+                <Field 
+                className='sign-up-form__password' 
+                placeholder='Enter Password' 
+                component={FormInput} 
+                name='password' 
+                title="Password"
+                type='password' />
+                <Field 
+                className='sign-up-form__create-account' 
+                component={FormButton} 
+                name='createaccount' 
+                title="Create Account"
+                type='submit' />
+
+                <div className="sign-up-form__text-links">
+                    <TextLink to='/signin' text='Already Registered? Login'/>
+                </div>
+            </form>
+        )
+    }
+}
+
+SignupForm = reduxForm({
+    form: 'signup'
+})(SignupForm);
+
+export default SignupForm;
