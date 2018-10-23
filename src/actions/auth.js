5d3c79c314c191df6b89c23520b2d6efc7e256d3
joshh450 <joshh450@yahoo.com>
2018-10-23 16:34:38 -0600

    handled signup post request

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -3,12 +3,13 @@ import axios from 'axios';
 export function signUp(fields, success) {
     console.log(fields);
     return function(dispatch) {
-        axios.post(`${ROOT_URL}/signUp`)
-            // .then(response => {
-
-            // })
-            // .catch(err => {
-            //     if(err) {console.log(oops)}
-            // })
+        axios.post(`${ROOT_URL}/signUp`, fields)
+            .then(response => {
+               console.log(response);
+               success()
+            })
+            .catch(err => {
+                if(err) { console.log(err) }
+            })
     }
 }
--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+
 import SignupForm from './signupForm';
 
 class Signup extends Component {
 
     onSubmit = (fields) => {
-        
+        this.props.signUp(fields, () => {
+            console.log('navigate to dashboard');
+            this.props.history.push('/dashboard');
+        })
     }
 
 
@@ -18,4 +24,4 @@ class Signup extends Component {
     }
 }
 
-export default Signup;
+export default connect(null, actions)(Signup);
